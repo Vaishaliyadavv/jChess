384700ec30ca49a23d6ab5540b35584eef648624
Vaishaliyadavv <vaishali.yadavv09@gmail.com>
2024-04-01 21:03:59 +0530

    add to calculateLegalMove

--- src/main/java/com/chess/engine/pieces/Pawn.java
+++ src/main/java/com/chess/engine/pieces/Pawn.java
@@ -2,6 +2,7 @@ package com.chess.engine.pieces;
 
 import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
 import com.chess.engine.board.Move;
 
 import java.util.ArrayList;
@@ -11,11 +12,31 @@ import java.util.List;
 public class Pawn extends Piece{
     private final int[] CANDIDATE_MOVE_COORDINATE ={8};
     Pawn(int piecePosition, Alliance pieceAlliance) {
+
         super(piecePosition, pieceAlliance);
     }
 
     @Override
     public Collection<Move> calculateLegalMove(Board board) {
+
         final List<Move> legalMoves = new ArrayList<>();
+
+        for (final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATE) {
+            int candidateDestinationCoordinate =
+                    this.piecePosition + (this.getPieceAlliance().getDirection()
+                            * currentCandidateOffset);
+
+            if (!BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)){
+                continue;
+            }
+            if (currentCandidateOffset == 8 &&
+                    !board.getTile(candidateDestinationCoordinate).isTileOccupied()){
+                //TODO more work to do here
+                legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate));
+
+            }
+        }
+        
+        return legalMoves;
     }
 }
