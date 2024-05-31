4496b31a065dffb1725e997f9869f1afcd0aa644
Vaishaliyadavv <vaishali.yadav09@gmail.com>
2024-05-31 21:08:16 +0530

    add calculateAttackOnTile in Player.java

--- src/main/java/com/chess/engine/player/Player.java
+++ src/main/java/com/chess/engine/player/Player.java
@@ -5,19 +5,35 @@ import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
 import com.chess.engine.pieces.King;
 import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableList;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 public abstract class Player {
 
     protected final Board board;
     protected final King playerKing;
     protected final Collection<Move> legalMoves;
+    private final boolean isInCheck;
 
     Player(final Board board, final Collection<Move> legalMoves, final Collection<Move> opponentMoves) {
         this.board = board;
         this.playerKing = establishKing();
         this.legalMoves = legalMoves;
+        this.isInCheck = !Player.calculateAttackOnTile(this.playerKing.getPiecePosition(), opponentMoves).isEmpty();
+    }
+
+    private static Collection<Move> calculateAttackOnTile(int piecePosition, Collection<Move> moves) {
+        final List<Move> attackMoves = new ArrayList<>();
+        for (final Move move : moves) {
+            if (piecePosition == move.getDestinationCoordinate()){
+                attackMoves.add(move);
+            }
+        }
+        return ImmutableList.copyOf(attackMoves);
     }
 
     private King establishKing() {
@@ -36,7 +52,7 @@ public abstract class Player {
     //TODO implement these methods below
 
     public boolean isInCheck() {
-        return false;
+        return this.isInCheck;
     }
 
     public boolean isInCheckMate() {
@@ -47,10 +63,11 @@ public abstract class Player {
         return false;
     }
 
-    public boolean isCastled(){
+    public boolean isCastled() {
         return false;
     }
-    public MoveTransition makeMove(final Move move){
+
+    public MoveTransition makeMove(final Move move) {
         return null;
     }
 
